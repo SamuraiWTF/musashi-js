f01b18adc5773ed36bc71f5f569fbb29cc798688
Mic <mic@secureideas.com>
2019-02-25 22:41:42 -0500

    Oauth server implementation

--- provider.oauth.demo/app.js
+++ provider.oauth.demo/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const OAuthServer = require('express-oauth-server');
 const app = express();
+const LokiModel = require('./models/LokiModel');
+
+app.oauth = new OAuthServer({ model: new LokiModel(), debug: true });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false }));
+app.use(app.oauth.authorize());
 
 app.get('/', (req, res) => res.send('Hello OAuth Provider!'))
 
---
+++ provider.oauth.demo/models/LokiModel.js
@@ -0,0 +1,50 @@
+const loki = require("lokijs");
+
+class LokiModel {
+    constructor() {
+        // This would be an issue if multiple we instantiated.
+        this.db = new loki(`oauth_${LokiModel.__count}.json`);
+        this.db.addCollection("clients");
+        this.db.addCollection("tokens");
+        this.db.addCollection("users");
+
+        LokiModel.__count++;
+    }
+
+    dump() {
+        console.log('clients', this.db.clients);
+        console.log('tokens', this.db.clients);
+        console.log('users', this.db.users);
+    }
+
+    getAccessToken(bearerToken) {
+        return this.db.tokens.findOne({ accessToken: bearerToken });
+    }
+
+    getClient(clientId, clientSecret) {
+        return this.db.clients.findOne({ clientId: clientId, clientSecret: clientSecret})
+    }
+
+    getRefreshToken(refreshToken) {
+        return this.db.tokens.findOne({ refreshToken: refreshToken });
+    }
+
+    getUser(username, password) {
+        return this.db.users.findOne({ username: username, password: password });
+    }
+
+    saveToken(token, client, user) {
+        this.db.tokens.insert({
+            accessToken: token.accessToken,
+            accessTokenExpiresAt: token.accessTokenExpiresAt,
+            clientId: client.clientId,
+            refreshToken: token.refreshToken,
+            refreshTokenExpiresAt: token.refreshTokenExpiresAt,
+            userId: user.id
+          });
+    }
+}
+
+LokiModel.__count = 0;
+
+module.exports = LokiModel;
