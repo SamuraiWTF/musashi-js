8c0a386ee4b501e7e1ed69655157cf55a1383a77
Mic <mic@secureideas.com>
2019-03-13 20:06:46 -0700

    Added test suite for api.cors.demo

---
+++ test/api.cors.demo/controllers.js
@@ -0,0 +1,10 @@
+const cookieAuth = require('../../api.cors.demo/controllers/cookieAuth')
+const data = require('../../api.cors.demo/controllers/data')
+
+describe('#cookieAuth', function () {
+  it.skip('Should have tests')
+})
+
+describe('#data', function () {
+  it.skip('Should have tests')
+})
---
+++ test/api.cors.demo/middleware.js
@@ -0,0 +1,12 @@
+const cookie = {
+  bouncer: require('../../api.cors.demo/middleware/cookieSessionBouncer'),
+  loader: require('../../api.cors.demo/middleware/cookieSessionLoader')
+}
+
+describe('#cookieSessionBouncer', function () {
+  it.skip('Should have tests')
+})
+
+describe('#cookieSessionLoader', function () {
+  it.skip('Should have tests')
+})
