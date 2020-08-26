5f7f92b38267f07b828edb907f3ec1e098866d19
Mic <mic@secureideas.com>
2020-08-25 20:39:21 -0400

    Added configurability for CORS API host through settings

--- client.cors.demo/app.js
+++ client.cors.demo/app.js
@@ -5,6 +5,12 @@ const app = express()
 const jucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('client.cors.demo/views'))
 
 const apiHost = process.env.CORS_API_HOST || 'api.cors.dem';
+const protocol = stringToBool(process.env.USE_TLS) ? 'https' : 'http';
+
+// Escape backticks - for dumping variables into template literals on the front-end.
+jucksEnv.addFilter("escbt", (str) => {
+  return str.replace(/`/g, '\\`');
+});
 
 jucksEnv.express(app)
 app.set('view engine', 'njk')
@@ -12,7 +18,11 @@ app.set('view engine', 'njk')
 app.use(express.static('client.cors.demo/static'))
 
 app.get('/', (req, res) => {
-    res.render('index', { apiHost: apiHost, protocol: stringToBool(process.env.USE_TLS) ? 'https' : 'http' })
+    res.render('index', { apiHost: apiHost, protocol: protocol })
+})
+
+app.get('/settings', (req, res) => {
+    res.render('settings', { apiHost: apiHost, protocol: protocol })
 })
 
 module.exports = app
