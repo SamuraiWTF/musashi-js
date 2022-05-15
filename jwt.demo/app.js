478a1fbfc9371463557d92ceec11c936c6d8734d
mgillam <mic@secureideas.com>
2022-05-14 20:19:29 -0400

    JWT wip

--- jwt.demo/app.js
+++ jwt.demo/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const stringToBool = require('../common/stringToBool')
+const jwt = require('jsonwebtoken')
 
 const app = express()
+const api = express.Router()
 const jucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader('jwt.demo/views'))
 
 const host = process.env.JWT_HOST || 'jwt.demo';
@@ -21,5 +23,21 @@ app.use(express.static('jwt.demo/static'))
 app.get('/', (req, res) => {
     res.render('index', { apiHost: host, protocol: protocol })
 })
+
+api.use(express.json())
+
+api.post('/sign', (req, res) => {
+  const { header, body, secret } = req.body;
+  const alg = header.alg.toUpperCase()
+  const payload = JSON.stringify(body)
+  const token = jwt.sign(payload, secret, { algorithm: alg })
+  res.status(200).send({ token: token })
+})
+
+api.post('/fuzz', (req, res) => {
+  res.status(200).send({hello: 'world'})
+})
+
+app.use('/api', api)
 
 module.exports = app
